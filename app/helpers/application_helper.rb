c417ed752af038d5647d65768d5cac6c14a5af36
Jason Ronallo <jronallo@gmail.com>
2011-11-06 18:05:02 -0500

    create helper for finding incomplete achievements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper 
+  
+  def incomplete_achievements(profile)
+    Achievement.subclasses.select do |klazz|
+      true if !profile.send(klazz.to_s.parameterize)
+    end
+  end
+  
 end
