9a926c8d7114a3a1a73489a879a62b82be5353ff
Jason Ronallo <jronallo@gmail.com>
2012-02-18 15:39:42 -0500

    make categories into buttons

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,2 +1,8 @@
 module ProfilesHelper
+  
+  def link_to_category(category)
+    link_text = "#{category['_id']} (#{category['value'].to_i})" 
+    link_to link_text, profiles_path(:category => category['_id']), 
+      :class => [:btn, 'btn-small', 'btn-primary', :category]
+  end
 end
