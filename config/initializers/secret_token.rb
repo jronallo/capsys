db2b75e6c33ac87cc0b866dbbbe1654a50119160
Jason Ronallo <jronallo@gmail.com>
2012-02-19 12:37:25 -0500

    secret token is now set through an environmental variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DplaCaps::Application.config.secret_token = '4b74e04ee0e3a2dfb6c90ffba6a017ce77c7ad6fabdc205c3c1699ac2228b7406ac7840d663bfbe04c358cee93e49a45e54b42ab515dc24e02ba8d3b7f191e03'
+DplaCaps::Application.config.secret_token = ENV['SECRET_TOKEN']
